fbefbbf4c516fd8c51ed1c15b2d76347eed0c50c
Daniel <webmaster@ltspiceusers.ch>
2022-09-27 01:23:20 +0200

    Add files via upload

---
+++ dxf2gcode/globals/constants.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+
+############################################################################
+#
+#   Copyright (C) 2010-2015
+#    Christian Kohlöffel
+#    Jean-Paul Schouwstra
+#
+#   This file is part of DXF2GCODE.
+#
+#   DXF2GCODE is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
+#
+#   DXF2GCODE is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with DXF2GCODE.  If not, see <http://www.gnu.org/licenses/>.
+#
+############################################################################
+
+"""
+All global constants are initialized in this module.
+They are used in the other modules.
+
+see http://code.activestate.com/recipes/65207/ for module const
+
+@purpose:  initialization of the global constants used within the other modules.
+"""
+
+import logging
+
+import platform
+
+from PyQt5 import QtCore
+
+# Global Variables
+APPNAME = "DXF2GCODE"
+VERSION = "Py%s PyQt%s" % (platform.python_version(), QtCore.PYQT_VERSION_STR)
+
+DATE     =  "$Date: 2022-02-26 15:29 CET $"
+REVISION =  "$Revision: b6983e9e9c3df532b2f621ab4e89dc813b521bd7 $"
+AUTHOR   = u"$Author: Christian Kohloeffel <christian.kohloeffel@googlemail.com> $"
+
+CONFIG_EXTENSION = '.cfg'
+PY_EXTENSION = '.py'
+PROJECT_EXTENSION = '.d2g'
+
+# Rename unreadable config/varspace files to .bad
+BAD_CONFIG_EXTENSION = '.bad'
+DEFAULT_CONFIG_DIR = 'config'
+DEFAULT_POSTPRO_DIR = 'postpro_config'
+
+# log related
+DEFAULT_LOGFILE = 'dxf2gcode.log'
+STARTUP_LOGLEVEL = logging.DEBUG
+# PRT = logging.INFO
